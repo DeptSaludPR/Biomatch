9f74e00df31ee94defe2e57c63c849c7c7e67caf
Elvis Nieves <elvis.nievesmiranda@gmail.com>
2022-09-03 02:29:45 -0400

    feat(nullable): add nullable annotations and remove unnecessary checks in loop (#12)

--- HelperClasses.cs
+++ HelperClasses.cs
@@ -1,11 +1,11 @@
-using System;
-
 namespace MatchingEngine;
 
 public static class Helpers
 {
     public static bool FirstCharactersAreEqual(string a, string b)
     {
+        if (a == string.Empty || b == string.Empty)
+            return true;
         return string.Equals(a[..1], b[..1], StringComparison.InvariantCultureIgnoreCase);
     }
 }
