e8d7330665496be31db84791099a8e30ad3ffdd5
Elvis Nieves Miranda <elvis.nievesmiranda@gmail.com>
2022-09-10 01:26:27 -0400

    feat(cli): add template validate command

--- MatchingEngine.CLI/Commands/MatchingCommand.Templates.cs
+++ MatchingEngine.CLI/Commands/MatchingCommand.Templates.cs
@@ -9,6 +9,20 @@ public static partial class MatchingCommand
 {
     public static Command GetTemplateCommand()
     {
+        var templateGenerateCommand = GetTemplateGenerateCommand();
+        var templateValidateCommand = GetTemplateValidateCommand();
+
+        var command = new Command("template", "Template operations")
+        {
+            templateGenerateCommand,
+            templateValidateCommand
+        };
+
+        return command;
+    }
+
+    private static Command GetTemplateGenerateCommand()
+    {
         var outputOption = new Option<FileInfo>
         (name: "--output", description: "Output file path",
             getDefaultValue: () => new FileInfo("MatchingTemplate.csv"));
@@ -26,12 +40,38 @@ public static partial class MatchingCommand
                 await using var csv = new CsvWriter(writer, CultureInfo.InvariantCulture);
                 csv.WriteHeader<PatientRecord>();
             }, outputOption);
-        
-        var rootCommand = new Command("template", "Template operations")
+
+        return command;
+    }
+
+    private static Command GetTemplateValidateCommand()
+    {
+        var templateFilePathArgument = new Argument<FileInfo>
+            (name: "template file path", description: "The path of the file to be validated");
+
+        var command = new Command("validate", "Validates template format")
         {
-            command
+            templateFilePathArgument,
         };
 
-        return rootCommand;
+        command.SetHandler(
+            (templateFilePathArgumentValue) =>
+            {
+                try
+                {
+                    using var readerFile1 = new StreamReader(templateFilePathArgumentValue.FullName);
+                    using var csvRecords1 = new CsvReader(readerFile1, CultureInfo.InvariantCulture);
+                    var records = csvRecords1.GetRecords<PatientRecord>();
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"Template file is valid, and contains {records.Count():N0} records.");
+                }
+                catch (Exception)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Template file is invalid, use the template generate command to create a valid template file.");
+                }
+            }, templateFilePathArgument);
+
+        return command;
     }
 }
--- MatchingEngine.CLI/Services/DuplicateService.cs
+++ MatchingEngine.CLI/Services/DuplicateService.cs
@@ -1,3 +1,4 @@
+using System.Collections.Concurrent;
 using System.Diagnostics;
 using System.Globalization;
 using CsvHelper;
@@ -9,10 +10,11 @@ namespace MatchingEngine.CLI.Services;
 
 public static class DuplicateService
 {
-    public static async Task RunFileComparisons(IEnumerable<PatientRecord> records1, IEnumerable<PatientRecord> records2,
-        FileInfo outputFileName, bool searchAllFile1 = true, int startIndexFile1 = 1, int endIndexFile1 = 100,
-        bool searchAllFile2 = true, int startIndexFile2 = 1, int endIndexFile2 = 100, bool exactMatchesAllowed = false,
-        double lowerScoreThreshold = 0.65, FileInfo? logFilePath = null)
+    public static async Task RunFileComparisons(IEnumerable<PatientRecord> records1,
+        IEnumerable<PatientRecord> records2, FileInfo outputFileName, bool searchAllFile1 = true,
+        int startIndexFile1 = 1, int endIndexFile1 = 100, bool searchAllFile2 = true, int startIndexFile2 = 1,
+        int endIndexFile2 = 100, bool exactMatchesAllowed = false, double lowerScoreThreshold = 0.65,
+        FileInfo? logFilePath = null)
     {
         //start a stopwatch
         var timer = new Stopwatch();
@@ -20,7 +22,7 @@ public static class DuplicateService
 
         var preprocessedRecords1 = Preprocess.PreprocessData(records1).ToArray();
         var preprocessedRecords2 = Preprocess.PreprocessData(records2).ToArray();
-        
+
         //check if the user wants to search among all entities from record 1. 
         //If true, set start_index1 to 0. Else, use the provided start index
         var startIndex1 = searchAllFile1 ? 0 : startIndexFile1;
@@ -47,33 +49,8 @@ public static class DuplicateService
 
         if (logFilePath is not null)
         {
-            var logTs = timer.Elapsed;
-            var logElapsedTime =
-                $"{logTs.Hours:00}:{logTs.Minutes:00}:{logTs.Seconds:00}.{logTs.Milliseconds / 10:00}";
-        
-            //open a new log document 
-            var log = new StreamWriter(logFilePath.FullName);
-
-            //write the log start time and settings 
-            await log.WriteLineAsync("Log start time: " + DateTime.UtcNow.ToString("yyyy-MM-dd hh:mm:ss zzzzzz"));
-            await log.WriteLineAsync(
-                "\nThe variable 'exact_matches_allowed' is set to: " + exactMatchesAllowed);
-            await log.WriteLineAsync("\nFor this run, we are using a score threshold of: " + lowerScoreThreshold);
-            await log.WriteLineAsync("\nThe whole process took: " + logElapsedTime);
-            await log.WriteLineAsync($"\nThere are {preprocessedRecords1.Length} provided records for the sample.");
-            await log.WriteLineAsync($"\nThere are {preprocessedRecords2.Length} records to search for duplicates.");
-            await log.WriteLineAsync($"\nThere are {potentialDuplicates.Count} potential duplicates in the provided files. This represents {potentialDuplicates.Count / (double) preprocessedRecords2.Length:P2} of the provided records.");
-            await log.WriteLineAsync(
-                $"\n{potentialDuplicates.Count(x => x.Score >= 0.9)} have a score of 0.9 or higher. ({potentialDuplicates.Count(x => x.Score >= 0.9) / (double) potentialDuplicates.Count:P2})");
-            await log.WriteLineAsync(
-                $"\n{potentialDuplicates.Count(x => x.Score is >= 0.8 and < 0.9)} have a score of >= 0.8 and < 0.9. ({potentialDuplicates.Count(x => x.Score is >= 0.8 and < 0.9) / (double) potentialDuplicates.Count:P2})");
-            await log.WriteLineAsync(
-                $"\n{potentialDuplicates.Count(x => x.Score is >= 0.7 and < 0.8)} have a score of >= 0.7 and < 0.8. ({potentialDuplicates.Count(x => x.Score is >= 0.7 and < 0.8) / (double) potentialDuplicates.Count:P2})");
-
-            //get the current time 
-            await log.WriteLineAsync("\nLog close time: " + DateTime.UtcNow.ToString("yyyy-MM-dd hh:mm:ss zzzzzz"));
-
-            log.Close();
+            await WriteLogFile(logFilePath, timer.Elapsed, exactMatchesAllowed, lowerScoreThreshold,
+                preprocessedRecords1.Length, preprocessedRecords2.Length, potentialDuplicates);
         }
 
         var urlDocs = potentialDuplicates
@@ -82,5 +59,38 @@ public static class DuplicateService
         await using var csv = new CsvWriter(writer, CultureInfo.InvariantCulture);
         csv.Context.RegisterClassMap<DuplicateRecordMap>();
         await csv.WriteRecordsAsync(urlDocs);
+    }
+
+    private static async Task WriteLogFile(FileSystemInfo logFilePath, TimeSpan timeSpan, bool exactMatchesAllowed,
+        double lowerScoreThreshold, int sampleRecordsTotal, int recordsToSearchTotal,
+        ConcurrentBag<PotentialDuplicate> potentialDuplicates)
+    {
+        var logElapsedTime =
+            $"{timeSpan.Hours:00}:{timeSpan.Minutes:00}:{timeSpan.Seconds:00}.{timeSpan.Milliseconds / 10:00}";
+
+        //open a new log document 
+        var log = new StreamWriter(logFilePath.FullName);
+
+        //write the log start time and settings 
+        await log.WriteLineAsync("Log start time: " + DateTime.UtcNow.ToString("yyyy-MM-dd hh:mm:ss zzzzzz"));
+        await log.WriteLineAsync(
+            "\nThe variable 'exact_matches_allowed' is set to: " + exactMatchesAllowed);
+        await log.WriteLineAsync("\nFor this run, we are using a score threshold of: " + lowerScoreThreshold);
+        await log.WriteLineAsync("\nThe whole process took: " + logElapsedTime);
+        await log.WriteLineAsync($"\nThere are {sampleRecordsTotal:N0} provided records for the sample.");
+        await log.WriteLineAsync($"\nThere are {recordsToSearchTotal:N0} records to search for duplicates.");
+        await log.WriteLineAsync(
+            $"\nThere are {potentialDuplicates.Count:N0} potential duplicates in the provided files. This represents {potentialDuplicates.Count / (double) recordsToSearchTotal:P2} of the provided records.");
+        await log.WriteLineAsync(
+            $"\n{potentialDuplicates.Count(x => x.Score >= 0.9):N0} have a score of 0.9 or higher. ({potentialDuplicates.Count(x => x.Score >= 0.9) / (double) potentialDuplicates.Count:P2})");
+        await log.WriteLineAsync(
+            $"\n{potentialDuplicates.Count(x => x.Score is >= 0.8 and < 0.9):N0} have a score of >= 0.8 and < 0.9. ({potentialDuplicates.Count(x => x.Score is >= 0.8 and < 0.9) / (double) potentialDuplicates.Count:P2})");
+        await log.WriteLineAsync(
+            $"\n{potentialDuplicates.Count(x => x.Score is >= 0.7 and < 0.8):N0} have a score of >= 0.7 and < 0.8. ({potentialDuplicates.Count(x => x.Score is >= 0.7 and < 0.8) / (double) potentialDuplicates.Count:P2})");
+
+        //get the current time 
+        await log.WriteLineAsync("\nLog close time: " + DateTime.UtcNow.ToString("yyyy-MM-dd hh:mm:ss zzzzzz"));
+
+        log.Close();
     }
 }
