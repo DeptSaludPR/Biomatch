11223478ad9c84649532579b86dfbee7ed2df6f2
Elvis Nieves Miranda <elvis.nievesmiranda@gmail.com>
2023-04-17 17:23:15 -0400

    feat: optimize dictionary letter creation to avoid dictionary lookups

--- src/Biomatch.Domain/Match.cs
+++ src/Biomatch.Domain/Match.cs
@@ -1,14 +1,15 @@
 using System.Collections.Concurrent;
+using System.Runtime.CompilerServices;
 using Biomatch.Domain.Models;
 
 namespace Biomatch.Domain;
 
 public static class Match
 {
-  public static IEnumerable<PotentialMatch> TryMatchSingleRecord(IEnumerable<PatientRecord> records1,
+  public static IEnumerable<PotentialMatch> FindBestMatches(IEnumerable<PatientRecord> records1,
     IEnumerable<PatientRecord> records2, double matchScoreThreshold, WordDictionary? firstNamesDictionary = null,
     WordDictionary? middleNamesDictionary = null, WordDictionary? lastNamesDictionary = null,
-    IProgress<int>? progress = null)
+    IProgress<int>? matchProgressReport = null)
   {
     var preprocessedRecords1 =
       records1.PreprocessData(firstNamesDictionary, middleNamesDictionary, lastNamesDictionary).ToArray();
@@ -16,7 +17,7 @@ public static class Match
       records2.PreprocessData(firstNamesDictionary, middleNamesDictionary, lastNamesDictionary).ToArray();
 
     var recordMatchResults =
-      GetPotentialMatches(preprocessedRecords1, preprocessedRecords2, matchScoreThreshold, 1.0, progress);
+      GetPotentialMatches(preprocessedRecords1, preprocessedRecords2, matchScoreThreshold, 1.0, matchProgressReport);
 
     var matchedRecords = recordMatchResults
       .GroupBy(x => x.Value)
@@ -29,27 +30,31 @@ public static class Match
 
   public static ConcurrentBag<PotentialMatch> GetPotentialMatches(Memory<PatientRecord> records1,
     Memory<PatientRecord> records2, double lowerScoreThreshold, double upperScoreThreshold,
-    IProgress<int>? progress = null)
+    IProgress<int>? matchProgressReport = null)
   {
     var potentialMatches = new ConcurrentBag<PotentialMatch>();
 
     var records1CharacterStartAndEndIndex = GetCharactersStartAndEndIndex(records1.Span);
     var records2CharacterStartAndEndIndex = GetCharactersStartAndEndIndex(records2.Span);
 
-    // Maximum of 27 iterations because of letters in the alphabet
-    Parallel.ForEach(records1CharacterStartAndEndIndex, record1StartAndEnd =>
+    var parallelOptions = new ParallelOptions {MaxDegreeOfParallelism = Environment.ProcessorCount};
+    // Maximum of 26 iterations because of letters in the alphabet
+    Parallel.For(0, records1CharacterStartAndEndIndex.Length, parallelOptions, record1Index =>
     {
-      var records2StartAndEndFound =
-        records2CharacterStartAndEndIndex.TryGetValue(record1StartAndEnd.Key, out var records2StartAndEnd);
+      var record1StartAndEnd = records1CharacterStartAndEndIndex[record1Index];
+      // If the start index is -1, skip this iteration
+      if (record1StartAndEnd.Item1 == -1)
+        return;
+      var records2StartAndEnd = records2CharacterStartAndEndIndex[record1Index];
 
-      var records1ToCompare = records1.Slice(record1StartAndEnd.Value.Item1,
-        record1StartAndEnd.Value.Item2 - record1StartAndEnd.Value.Item1 + 1);
+      var records1ToCompare =
+        records1.Slice(record1StartAndEnd.Item1, record1StartAndEnd.Item2 - record1StartAndEnd.Item1 + 1);
+      var records2ToCompare = records2StartAndEnd.Item1 == -1
+        ? records2
+        : records2.Slice(records2StartAndEnd.Item1, records2StartAndEnd.Item2 - records2StartAndEnd.Item1 + 1);
 
-      var records2ToCompare = records2StartAndEndFound
-        ? records2.Slice(records2StartAndEnd.Item1, records2StartAndEnd.Item2 - records2StartAndEnd.Item1 + 1)
-        : records2;
       // For each record in the first table, compare it to all records in the second table
-      Parallel.For(0, records1ToCompare.Length, recordToCompareIndex =>
+      Parallel.For(0, records1ToCompare.Length, parallelOptions, recordToCompareIndex =>
       {
         for (var i = 0; i < records2ToCompare.Length; i++)
         {
@@ -58,8 +63,59 @@ public static class Match
             lowerScoreThreshold, upperScoreThreshold);
         }
 
-        progress?.Report(1);
+        matchProgressReport?.Report(1);
       });
+    });
+
+    return potentialMatches;
+  }
+
+  public static ConcurrentBag<PotentialMatch> GetPotentialMatchesV2(Memory<PatientRecord> records1,
+    Memory<PatientRecord> records2, double lowerScoreThreshold, double upperScoreThreshold,
+    IProgress<int>? progress = null)
+  {
+    var potentialMatches = new ConcurrentBag<PotentialMatch>();
+
+    var records1CharacterStartAndEndIndex = GetCharactersStartAndEndIndex(records1.Span);
+    var records2CharacterStartAndEndIndex = GetCharactersStartAndEndIndex(records2.Span);
+
+    ConcurrentDictionary<PatientRecord, Memory<PatientRecord>> recordSlicesToCompare = new();
+
+    var parallelOptions = new ParallelOptions {MaxDegreeOfParallelism = Environment.ProcessorCount};
+
+    // Maximum of 26 iterations because of letters in the alphabet
+    Parallel.For(0, records1CharacterStartAndEndIndex.Length, parallelOptions, record1Index =>
+    {
+      var record1StartAndEnd = records1CharacterStartAndEndIndex[record1Index];
+      // If the start index is -1, skip this iteration
+      if (record1StartAndEnd.Item1 == -1)
+        return;
+      var records2StartAndEnd = records2CharacterStartAndEndIndex[record1Index];
+
+      var records1ToCompare =
+        records1.Slice(record1StartAndEnd.Item1, record1StartAndEnd.Item2 - record1StartAndEnd.Item1 + 1);
+      var records2ToCompare = records2StartAndEnd.Item1 == -1
+        ? records2
+        : records2.Slice(records2StartAndEnd.Item1, records2StartAndEnd.Item2 - records2StartAndEnd.Item1 + 1);
+
+      // For each record in the first table, compare it to all records in the second table
+      for (var recordToCompareIndex = 0; recordToCompareIndex < records1ToCompare.Length; recordToCompareIndex++)
+      {
+        recordSlicesToCompare.TryAdd(Unsafe.AsRef(records1ToCompare.Span[recordToCompareIndex]), records2ToCompare);
+      }
+    });
+
+
+    var partitionerRecordsToCompare = Partitioner.Create(recordSlicesToCompare);
+    Parallel.ForEach(partitionerRecordsToCompare, parallelOptions, recordToCompare =>
+    {
+      for (var i = 0; i < recordToCompare.Value.Length; i++)
+      {
+        CompareRecords(potentialMatches, ref Unsafe.AsRef(recordToCompare.Key), ref recordToCompare.Value.Span[i],
+          lowerScoreThreshold, upperScoreThreshold);
+      }
+
+      progress?.Report(1);
     });
 
     return potentialMatches;
@@ -80,13 +136,15 @@ public static class Match
     }
   }
 
-  private static Dictionary<char, (int, int)> GetCharactersStartAndEndIndex(ReadOnlySpan<PatientRecord> records)
+  private static (int, int)[] GetCharactersStartAndEndIndex(ReadOnlySpan<PatientRecord> records)
   {
-    var characterIndex = new Dictionary<char, (int, int)>();
+    var characterIndex = new (int, int)[26]; // 26 letters in the alphabet
 
     var currentIndex = 0;
-    for (var letter = 'a'; letter <= 'z'; letter++)
+    for (var letterIndex = 0; letterIndex < 26; letterIndex++)
     {
+      var letter = (char) ('a' + letterIndex);
+
       int? startIndex = null;
       int? endIndex = null;
       for (var i = currentIndex; i < records.Length; i++)
@@ -104,8 +162,14 @@ public static class Match
         break;
       }
 
-      if (startIndex == null) continue;
-      characterIndex.TryAdd(letter, (startIndex.Value, endIndex ?? records.Length - 1));
+      if (startIndex != null)
+      {
+        characterIndex[letterIndex] = (startIndex.Value, endIndex ?? records.Length - 1);
+      }
+      else
+      {
+        characterIndex[letterIndex] = (-1, -1); // Using -1 for non-existing start and end indexes
+      }
     }
 
     return characterIndex;
